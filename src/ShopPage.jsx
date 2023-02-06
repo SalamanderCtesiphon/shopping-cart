53dab33c094876ac456a91500a409359f9cf0ba5
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-06 12:35:37 -0600

    added all of the book images for the shopping page

--- src/ShopPage.jsx
+++ src/ShopPage.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import bladerunner from "./images/bladerunner.jpeg";
+import catbook from "./images/catbook.jpeg";
+import crimeandpunishment from "./images/crimeandpunishment.jpeg";
+import dragon from "./images/dragon.jpeg";
+import neuromancer from "./images/neuromancer.jpeg";
+import warAndPeace from "./images/war_and_peace.jpeg";
 import './App.css';
 
 const ShopPage = () => {
@@ -9,7 +14,32 @@ const ShopPage = () => {
             id: 1,
             title: 'DoAndrioidsDreamOfElectricSheep',
             image: <img src={bladerunner} alt="bladerunner" style={{height: '20vh', width: '10vw'}}/>,
-        }
+        },
+        {
+            id: 2,
+            title: 'Schroedinger\'s Cat',
+            image: <img src={catbook} alt="schroedingers cat" style={{height: '20vh', width: '10vw'}}/>,
+        },
+        {
+            id: 3,
+            title: 'Crime and punishment',
+            image: <img src={crimeandpunishment} alt="crime and punishment" style={{height: '20vh', width: '10vw'}}/>,
+        },
+        {
+            id: 4,
+            title: 'Dragon Lance',
+            image: <img src={dragon} alt="dragon lance" style={{height: '20vh', width: '10vw'}}/>,
+        },
+        {
+            id: 5,
+            title: 'Neuromancer',
+            image: <img src={neuromancer} alt="nueromancer" style={{height: '20vh', width: '10vw'}}/>,
+        },
+        {
+            id: 1,
+            title: 'War and Peace',
+            image: <img src={warAndPeace} alt="war and peace" style={{height: '20vh', width: '10vw'}}/>,
+        },
     ])
     return (
         <div className="page">
