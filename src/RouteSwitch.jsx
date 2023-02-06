a5a9460ba70ed200a04da2ab5fa0f4008c81c2c9
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-06 10:01:12 -0600

    removed unused file

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Profile from "./Profile";
 import ShopPage from "./ShopPage";
 
 const RouteSwitch = () => {
@@ -8,7 +7,6 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/shopPage" element={<ShopPage />} />
       </Routes>
     </BrowserRouter>
