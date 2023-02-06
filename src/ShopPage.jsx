f2ab508dab4c41c6da9e121f419bc6bbd335f7c6
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-06 12:13:49 -0600

    adding a list of books to the shopping page

--- src/ShopPage.jsx
+++ src/ShopPage.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import {bladerunner} from "./images/bladerunner.jpeg";
 
 const ShopPage = () => {
+    const [books, setBooks] = useState([
+        {
+            id: 1,
+            title: 'DoAndrioidsDreamOfElectricSheep',
+            image: <img src={bladerunner} alt="bladerunner"/>
+        }
+    ])
     return (
-        <div className="navBar">
-              <h1 className='title'>Forgotten Lore Book Shop</h1>
-        <div className='btn'>
-            <Link to="/">Home</Link>
-        </div>
+        <div className="page">
+            <div className="navBar">
+                    <h1 className='title'>Forgotten Lore Book Shop</h1>
+                <div className='btn'>
+                    <Link to="/">Home</Link>
+                </div>
+            </div>
+            {books.image}
         </div>
     )
 };
