1c7e37965d0d87a181ed87dfbb291028aaccc6f3
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-07 12:17:05 -0600

    add an input element that gives the user the ability to specify the quantity of the item that they would like to add to their cart

--- src/BookSubmitCard.jsx
+++ src/BookSubmitCard.jsx
@@ -3,6 +3,8 @@ const BookSubmitCard = ({ book, setAddToCart}) => {
         <div 
                 className="bookList"
             >{book.image}
+            <label>Quantity:</label>
+            <input type="number" name="quantity" id="quantity" min='0' step="1"></input>
             <div 
                 className="btn"
                 onClick={() => setAddToCart(null)}
