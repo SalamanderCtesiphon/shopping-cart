68c909dc15004de00b69f98e29ab1a6bc6103f0c
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-08 13:10:08 -0600

    changed the basename to /

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -4,7 +4,7 @@ import ShopPage from "./ShopPage";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename="/app">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shopPage" element={<ShopPage />} />
